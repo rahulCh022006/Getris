e6bacedda6dbf517c031d6d46666825896dd12c7
rahulCh022006 <challapallirahul12@gmail.com>
2025-01-21 11:29:09 +0530

    Mech: HUD, Scoring and GameOver
    Added the Headsup display added a few pastel colors for different texts and buttons.
    
    Scoring is based on clearing a row.
    
    Gameover is trigered when piece stack up over the limit.

--- world.gd
+++ world.gd
@@ -47,8 +47,10 @@ var shapes := [i, t, o, z, s, l, j]
 var shapes_full := shapes.duplicate()
 
 #Grid Vars
-const rows := 20
-const columns := 10
+const ROWS := 20
+const COLUMNS := 10
+var score: int
+const ACCEL: float = 0.05
 
 
 #TileMap Variables
@@ -56,9 +58,12 @@ var tile_id : int = 0
 var piece_atlas : Vector2i
 var next_piece_atlas : Vector2i
 
-# Layers
+#Layers
 @onready var board: TileMapLayer = $Board
 @onready var game: TileMapLayer = $Game
+@onready var hud: CanvasLayer = $Hud
+
+
 
 #Movement Variables
 const start_position:= Vector2i(5,1)
@@ -67,6 +72,7 @@ const directions : Array = [Vector2i.LEFT, Vector2i.RIGHT, Vector2i.DOWN]
 var speed: float
 var steps :Array
 const steps_req: int = 50
+var game_running: bool = true
 
 #Piece Variables
 var piece_type
@@ -77,23 +83,38 @@ var active_piece: Array
 
 func _ready() -> void:
 	new_game()
-	
+	hud.get_node("NewGameButton").connect("pressed", new_game)
+	hud.get_node("QuitButton").connect("pressed", quit_game)
+func quit_game():
+	get_tree().quit()
 func new_game():
+	game_running = true
+	clear_panel()
+	clear_board()
+	score = 0
+	hud.get_node("ScoreLabel").text = "SCORE: " + str(score)
+	hud.get_node("GameOverLabel").hide()
 	speed = 1.0
 	steps = [0,0,0] # 0 - Left, 1 - Right, 2 - Down
 	piece_type = pick_piece()
 	piece_atlas = Vector2i(shapes_full.find(piece_type), 0)
+	next_piece_type = pick_piece()
+	next_piece_atlas = Vector2i(shapes_full.find(next_piece_type), 0)
 	create_piece()
 
 func pick_piece():
-	#randomize pickikng a piece
-	var piece
 	shapes.shuffle()
 	if not shapes.is_empty():
-		piece = shapes.pop_front()
+		return shapes.pop_front()
 	else:
 		shapes = shapes_full.duplicate()
-	return piece
+		return shapes.pop_front()  # Ensure a piece is returned after duplication
+
+func clear_board():
+	for i in range(ROWS):
+		for j in range(COLUMNS):
+			game.erase_cell(Vector2i(j + 1, i + 1))
+			board.erase_cell(Vector2i(j + 1, i + 1))
 
 func create_piece():
 	#Creation of the piece in the beginning or after a piece is dropped
@@ -102,6 +123,7 @@ func create_piece():
 	current_position = start_position
 	active_piece = piece_type[rotation_index] #Calling for a new active piece through pick_piece()
 	draw_piece(active_piece,current_position,piece_atlas)
+	draw_piece(next_piece_type[0], Vector2i(15,6), next_piece_atlas)
 	
 func clear_piece():
 	#Erase the tiles based on the active piece
@@ -109,44 +131,108 @@ func clear_piece():
 		game.erase_cell(current_position + i)
 	
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("DOWN"):
-		steps[2] += 10
-	elif Input.is_action_pressed("LEFT"):
-		steps[0] += 10
-	elif Input.is_action_pressed("RIGHT"):
-		steps[1] += 10
-	
-	#Movement downwards
-	steps[2] += speed
-	
-	#Movement in general
-	for i in range(steps.size()):
-		if steps[i] > steps_req:
-			move_piece(directions[i])
-			steps[i] = 0
+	if game_running:
+		if Input.is_action_pressed("DOWN"):
+			steps[2] += 10
+		elif Input.is_action_pressed("LEFT"):
+			steps[0] += 10
+		elif Input.is_action_pressed("RIGHT"):
+			steps[1] += 10
+		elif Input.is_action_just_pressed("DROP"):
+			rotate_piece()
+		
+		#Movement downwards
+		steps[2] += speed
+		#Movement in general
+		for i in range(steps.size()):
+			if steps[i] > steps_req:
+				move_piece(directions[i])
+				steps[i] = 0
 
 func move_piece(dir):
 	if can_move(dir):
 		clear_piece() #Remove the piece at the location and then
 		current_position += dir #Change the position then
 		draw_piece(active_piece, current_position, piece_atlas) #Draw a new piece at the new position
+	else:
+		if dir == Vector2i.DOWN:
+			land_piece()
+			check_rows()
+			piece_type = next_piece_type
+			piece_atlas = next_piece_atlas
+			next_piece_type = pick_piece()
+			next_piece_atlas = Vector2i(shapes_full.find(next_piece_type), 0)
+			clear_panel()
+			create_piece()
+			check_game_over()
+func check_rows():
+	var row: int = ROWS
+	while row > 0:
+		var count: int = 0
+		for i in range(COLUMNS):
+			if not is_free(Vector2i(i + 1,row)):
+				count += 1
+		if count == COLUMNS:
+			shift_rows(row)
+			score += 100
+			speed += ACCEL
+		else:
+			row -= 1
+	hud.get_node("ScoreLabel").text = "SCORE: " + str(score)
+	print("Score: \n", score , "Speed: ", speed)
+func shift_rows(row):
+	var atlas
+	for i in range(row, 1, -1):
+		for j in range(COLUMNS):
+			atlas = board.get_cell_atlas_coords(Vector2i(j + 1,i - 1))
+			if atlas == Vector2i(-1,-1):
+				board.erase_cell(Vector2i(j + 1, i))
+			else:
+				board.set_cell(Vector2i(j + 1 , i), tile_id, atlas)
+func clear_panel():
+	for i in range(14,19):
+		for j in range(5,9):
+			game.erase_cell(Vector2i(i,j))
+	
+func land_piece():
+	for i in active_piece:
+		game.erase_cell(current_position + i)
+		board.set_cell(current_position + i, tile_id, piece_atlas)
+
+func rotate_piece():
+	if can_rotate():
+		clear_piece()
+		rotation_index = (rotation_index + 1) % 4
+		active_piece = piece_type[rotation_index]
+		draw_piece(active_piece, current_position, piece_atlas)
 
 func can_move(dir):
 	#Checking if we can move
-	
 	var cm: bool = true
 	for i in active_piece:
-		print(is_free(current_position + i + dir))
 		if not is_free(current_position + i + dir):
 			cm = false
-	
 	return cm
+
+func can_rotate():
+	var cr :bool = true
+	var temp_rotation_index := (rotation_index + 1) % 4
+	for i in piece_type[temp_rotation_index]:
+		if not is_free(i + current_position):
+			cr = false
+	return cr
 
 func is_free(pos):
 	return board.get_cell_source_id(pos) == -1
+
+func check_game_over():
+	for i in active_piece:
+		if not is_free(i + current_position):
+			land_piece()
+			hud.get_node("GameOverLabel").show()
+			game_running = false
 
 func draw_piece(piece, pos, atlas):
 	# Iterate through piece array and draw a tile at each position
 	for i in piece:
 		game.set_cell(pos + i,tile_id, atlas)
-	
